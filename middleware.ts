60724b67516ace38f2fc479847e470b04f20892a
liem nguyen <elminguyen@gmail.com>
2024-04-13 13:26:28 +0300

    authentication start

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+import { updateSession } from "@/lib/authActions";
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,74 @@
-const Login = () => {
-  return (
-    <div>
-      <h2>Login</h2>
-    </div>
-  );
-};
+import { redirect } from 'next/navigation';
+import { getSession, login, logout } from '@/lib/authActions';
 
-export default Login;
+export default async function Page() {
+  // const session = await getSession();
+
+  return (
+    <section>
+      <div className="flex flex-col p-8">
+        {/* {!session ? ( */}
+        <form
+          action={async (formData) => {
+            'use server';
+            await login(formData);
+            redirect('/');
+          }}
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Login
+          </button>
+        </form>
+        {/* ) : ( */}
+        <form
+          action={async () => {
+            'use server';
+            await logout();
+            // redirect('/');
+          }}
+        >
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Logout
+          </button>
+        </form>
+        {/* )} */}
+      </div>
+    </section>
+  );
+}
