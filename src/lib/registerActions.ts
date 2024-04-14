67f283183242a0f0ee8482569f98dc81e38b4f35
liem nguyen <elminguyen@gmail.com>
2024-04-14 09:05:48 +0300

    register

--- src/lib/registerActions.ts
+++ src/lib/registerActions.ts
@@ -1,8 +1,32 @@
 'use server';
+import bcrypt from 'bcrypt';
 
 const doRegisterAction = async (formData: FormData) => {
-  const user = Object.fromEntries(formData);
-  console.log(user);
+  const userData = Object.fromEntries(formData);
+  // insert user to DB
+  // try {
+    const salt = bcrypt.genSaltSync(12);
+    console.log(userData);
+
+    userData.password = await bcrypt.hash(userData.password as string, salt);
+
+    console.log(userData);
+
+    // const newUser = await createUser(user);
+    // console.log('newUser', newUser);
+    // if (!newUser) {
+    //   next(new CustomError('User not created', 500));
+    //   return;
+    // }
+    // const response: UserResponse = {
+    //   message: 'user created',
+    //   user: newUser,
+    // };
+    // res.json(response);
+  // } catch (error) {
+    // next(new CustomError('Duplicate entry', 200));
+  // }
+  
 }
 
 export {doRegisterAction};
