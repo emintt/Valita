728d887c3e30e4448db19ad70c9c1c6e0f104fe8
liem nguyen <elminguyen@gmail.com>
2024-05-03 10:25:23 +0300

    update basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/app',
-  assetPrefix: '/app',
+  basePath: '/valita',
+  assetPrefix: '/valita',
   images: {
     remotePatterns: [
       {
